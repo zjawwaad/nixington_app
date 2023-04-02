38bbab2e784fce39ba34f755887a2088e50c3976
zjawwaad <zjawwaad@yahoo.com>
2023-04-02 12:11:42 -0400

    Update

--- flask_app/controllers/stories_controller.py
+++ flask_app/controllers/stories_controller.py
@@ -5,8 +5,8 @@ from flask_app.models.story_model import Story
 # Move to recipes controller 
 @app.route('/stories')
 def display_stories():
-    if 'email' not in session:
-        return redirect ('/')
+    # if 'email' not in session:
+    #     return redirect ('/')
     list_stories = Story.get_all_with_user()
     print(list_stories)
     return render_template('home.html', list_stories = list_stories)
